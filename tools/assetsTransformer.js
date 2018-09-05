99d1bdbc4d98e0e421b16ce255ac4ff63fe4017b
symesfin <symesfin001@gmail.com>
2018-09-04 22:51:04 -0500

    Update assetsTransformer.js

--- tools/assetsTransformer.js
+++ tools/assetsTransformer.js
@@ -10,5 +10,5 @@ const path = require('path');
 module.exports = {
   process(src, filename /*, config, options */) {
     return 'module.exports = ' + JSON.stringify(path.basename(filename)) + ';';
-  },
+  }
 };
